6e73974c917a16f6016feb0e9be52e248400ac98
vprogramer <57725344+vprogramer@users.noreply.github.com>
2023-05-24 09:57:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.helloworld = (req, res) => {
-  const message = "<font color='blue'>Cloud Function</font><br><b>App Version 1.0</b>";
+  const message = "<font color='blue'>Cloud Function</font><br><b>App Version 1.1</b>";
   res.status(200).send(message);
 };
